5fd5ed6acbd422a6420ae2a272e868a537a5a87c
eqwdy <fraksxx@gmail.com>
2026-08-19 02:52:41 +0300

    asd asd

--- client-next/src/shared/api/api.base.ts
+++ client-next/src/shared/api/api.base.ts
@@ -1,10 +1,10 @@
-import { SERVER_URL_CSR, SERVER_URL_SSR } from "@/shared/config/apiPaths";
+import { SERVER_URL_SSR } from "@/shared/config/apiPaths";
 import axios from "axios";
 
 export const apiSSR = axios.create({
   baseURL: SERVER_URL_SSR,
 });
 export const apiCSR = axios.create({
-  baseURL: SERVER_URL_CSR,
-  //   baseURL: `/api`,
+  //   baseURL: SERVER_URL_CSR,
+  baseURL: `/api`,
 });
